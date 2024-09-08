d7951438822a1a023d542c9db97e7bda6de568fd
KAN-JUNHO <junho6667@gmail.com>
2024-09-08 13:59:02 +0900

    init

---
+++ src/main/java/kr/co/onedayclass/OnedayclassApplication.java
@@ -0,0 +1,13 @@
+package kr.co.onedayclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnedayclassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnedayclassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/onedayclass/OnedayclassApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.onedayclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnedayclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
